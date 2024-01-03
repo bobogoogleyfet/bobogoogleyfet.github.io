74a40067a7a58c517cb240c0f54979525fc3743e
bobogoogleyfet <thathaway54@gmail.com>
2024-01-03 12:32:05 -0700

    centered the game

--- src/App.js
+++ src/App.js
@@ -34,23 +34,23 @@ export default function Board() {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="game-board">
-        <div className="board-row">
+      <table className="game-board">
+        <tr>
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
           <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
+        </tr>
+        <tr>
           <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
           <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
           <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
+        </tr>
+        <tr>
           <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
           <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
           <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </div>
+        </tr>
+      </table>
     </>
   );
 }
